f71b293878ea34c6d9430d651e286630fa7075cb
Deepanshipal00 <113660990+Deepanshipal00@users.noreply.github.com>
2025-03-15 07:43:54 +0000

    Array Functions Done

--- 01_Basics/Practice.js
+++ 01_Basics/Practice.js
@@ -1,73 +1,59 @@
-// Array
+const marvel = ["Ironman", "Thor", "Spiderman"];
+const dc = ["Superman", "Flash", "Batman"];
 
-const myArray = [0, 1, 2, 3, 4, 5, "Deepanshi", true]
-// A single variable can strore different datatype elements in an array form, they are resizable
+marvel.push(dc);
+console.log(marvel); // Array insert inside marvel an element (Nested array)
+// [ 'Ironman', 'Thor', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
+//Access Nested elements
 
-// To access an element form array
+console.log(marvel[3][1]); //Flash
 
-console.log(myArray[6])  // Deepanshi
+const marvel_heros = ["ironman", "spiderman", "thor", "C America"];
+const dc_heros = ["superman", "flash", "batman"];
 
-// Arraay copy operations create Shallow Copies(whose properties share the same reference (Heap Memory)) -> how data is duplicated and how modifications affect the original and copied structures.
+//Concat function returns a new array (doesn't merge like push)
 
-//Another way to declare array
-const myBooks = new Array("Thinking Fast & Slow", "When Breath Becomes Air", "Principles By Ray Dalio");
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+/*[
+  'ironman',
+  'spiderman',
+  'thor',
+  'C America',
+  'superman',
+  'flash',
+  'batman'
+]
+*/
+// Alternate Method -> Spread Operator
+const allNewHeros = [...marvel_heros, ...dc_heros];
+console.log(allNewHeros);  // returns same output given above
 
-console.log(myBooks);
-/* [
-  'Thinking Fast & Slow',
-  'When Breath Becomes Air',
-  'Principles By Ray Dalio'
-] */
-console.log(myBooks[2]);  // Principles By Ray Dalio
+const Arr1 = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const Arr2 = Arr1.flat(Infinity);
+// we can pass a value in flat method as the depth of its complexity(Nested elements) -> 3 or Infinity
+console.log(Arr2); //[1,2,3,4,5,6,7,4,5]
 
-//Array Methods
 
-myBooks.push("Deep Work", "Almanak of Naval Ravikant");  // push elements to an array
-myBooks.pop(); // Remove last elements from array
-myBooks.unshift("Big Debt Crises") // Insert an element at 0 index, but it shift rest of elements from their positions.
-myBooks.shift(); // Remove first element form array
-console.log(myBooks.length);  
-console.log(myBooks);
-console.log(myBooks.includes("Deep Work")); // true
-console.log(myBooks.indexOf("Hello World")); // -1 (When element doesn't exists)
-console.log(myBooks.indexOf("Deep Work"));  //3
+// Extraction and Conversion
 
-const newArr = myBooks.join();
-console.log(newArr); // Thinking Fast & Slow,When Breath Becomes Air,Principles By Ray Dalio,Deep Work
-console.log(typeof newArr); // String
+console.log(Array.isArray("Hello")); // False
+console.log(Array.from("Hello"));   // [ 'H', 'e', 'l', 'l', 'o' ] -> Convert into Array 
+console.log(Array.from({name : "Deepanshi"})); // []
+// plain objects are not iterable by default, so Array.from({ name: "Deepanshi" }) results in an empty array.
+// need to instruct for converstion otherwise it will return empty array
 
-// Slice and Splice
+console.log(Object.entries({ name: "Deepanshi", age : 22, city: "Noida"}));
+// Output: [ [ 'name', 'Deepanshi' ], [ 'age', 22 ], [ 'city', 'Noida' ] ]
 
-const myArr = new Array(1, 2, 3, 4, 5, 6);
-console.log(myArr);  // [ 1, 2, 3, 4, 5, 6 ]
+//we can also access keys and values saperately
 
-const myA1 = myArr.slice(1, 3);
-console.log("A", myArr);  // A [ 1, 2, 3, 4, 5, 6 ]
-console.log(myA1); // [ 2, 3 ]
 
-// Extracts a section of an array and returns a new array containing the extracted elements.
-// The original array remains unchanged.
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
 
-const myA2 = myArr.splice(1,3); 
-console.log("B", myArr);   // B [ 1, 5, 6 ]
-console.log(myA2);   // [ 2, 3, 4 ]
+console.log(Array.of(score1, score2, score3));  // [ 100, 200, 300 ]
 
-// Modifies the original array by removing or replacing existing elements and/or adding new elements. 
-
-let arr1 = [10, 20, 30, 40, 50];
-arr1.splice(1, 3);  // Removes 3 elements starting from index 1
-console.log(arr1);   // Output: [10, 50]
-
-let arr2 = [10, 50];
-arr2.splice(1, 0, 20, 30, 40);  // Inserts elements at index 1
-console.log(arr2);   // Output: [10, 20, 30, 40, 50]
-
-let arr3 = [10, 20, 30, 40, 50];
-arr3.splice(1, 2, 100, 200);  // Replaces 20 & 30 with 100 & 200
-console.log(arr3);   // Output: [10, 100, 200, 40, 50]
-
-let arr = [1, 2, 3, 4, 5];
-arr.splice(2);  // Removes everything from index 2 onward
-console.log(arr);  // Output: [1, 2]
 
 
