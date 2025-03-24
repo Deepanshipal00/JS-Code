0ba2de9aadcf579174feaeb3453a238bfa06403f
Deepanshipal00 <113660990+Deepanshipal00@users.noreply.github.com>
2025-03-24 18:36:32 +0000

    Control Flow Pending

--- 01_Basics/Practice.js
+++ 01_Basics/Practice.js
@@ -1,27 +1,49 @@
-// IIFE
-// IIFE (Immediately Invoked Function Expression) is used in JavaScript primarily to create a new scope. This helps avoid polluting the global namespace and manage variable scope effectively.
+// Control Flow or Logic Flow
 
-function code(){
-  console.log(`DB Connected`)
+// if
+// if(condition)-> If this condition is true code execute, if the condition is false code will not exceute {inside this scope}
+
+//Example
+const isUserLoggedIn = true;
+
+if(isUserLoggedIn){
+  console.log('Welcome Back!') // Welcome Back!
 }
 
-code(); // DB Connected
-
-function hello(){
-  console.log('Hello')
+const temperature = 41;
+if (temperature < 50){
+  console.log('Temperature is less than 50');  // Temperature is less than 50
+}else{
+console.log('Temperature is greater than 50'); // Not executed
 }
-hello(); // Hello
 
-// Named IIFE
-(function newCode(){
-  console.log(`DB Connected`)
-})();  // DB Connected
+if(temperature === 50){
+  console.log("Temperature is Hotter") // Condition is not true -> Not Executed
+}else{
+  console.log("Temperature is Rising") // This will execute -> Temperature is Rising
+}
+
+const score = 200;
+if(score >= 100){
+  const power = "Fly";
+  console.log(`Power : ${power}`); // Power : Fly
+}
+
+// Implicit Scope
+const balance = 1000;
+if(balance > 50) console.log("Test"); // Test
+
+// Nested Loops
+if(balance < 500){
+  console.log("Balance is less than 500");
+}else if(balance < 750){
+  console.log("Balance is less than 750");
+}else if(balance < 900){
+  console.log("Balance is less than 900");
+}else{
+  console.log("Balance is less than 1000 but greater than 900") // This will execute, above condition are not true
+}
 
 
-// Unnamend IIFE
-((name) => {
-  console.log(`Hello ${name}`)
-})('Deepanshi'); // Hello Deepanshi
-
-
-// We need to give Termination(in previous execution) to Invoke next function
+//comparision operator
+// <(less than), >(Greater than), <=(less than equals), >=(Greater than equals), =(assignment operator), ==(equals), !=(not equals), ===(type and value equals or strict equality operator), !== (not equal value or not equal type)
