e094c79b7afe0524a5e4eb3591153a7b32c00f86
Deepanshipal00 <113660990+Deepanshipal00@users.noreply.github.com>
2025-03-25 19:49:08 +0000

    Control Flow Done

--- 01_Basics/Practice.js
+++ 01_Basics/Practice.js
@@ -44,6 +44,131 @@ if(balance < 500){
   console.log("Balance is less than 1000 but greater than 900") // This will execute, above condition are not true
 }
 
+// Logical Operator AND &&, OR ||
 
+const userLogIn = true;
+const debitCard = true;
+const logInFromGoogle = false;
+const logInFromEmail = true;
+
+if (userLogIn && debitCard){
+  console.log("Allow to buy course"); // Allow to buy course
+}
+
+if(logInFromEmail || logInFromGoogle){
+  console.log("User Logged In");  // User Logged In
+}
+
+
+// && -> Both the condition must be true to execute the code
+// || -> One of the condition should be true to execute the code
 //comparision operator
 // <(less than), >(Greater than), <=(less than equals), >=(Greater than equals), =(assignment operator), ==(equals), !=(not equals), ===(type and value equals or strict equality operator), !== (not equal value or not equal type)
+
+//Switch
+
+/*
+***** Syntax *****
+switch(key) {
+      case value:
+        break;
+
+      default:
+        break;
+}
+*/
+
+// Shift + Alt + down arrow to duplicate
+const month = 3;
+
+switch(month){
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+  default:
+    console.log("Please enter Month number")
+    break;
+}
+//March
+// if break is not given/commented in 3rd condition where which is true, after that everything is executes except default condition
+
+// Truthy or Falsy Value
+const userEmail = "deepanshi@gmail.com";
+
+if (userEmail){
+  console.log(`Got user Email`); // User Logged In
+} else{
+  console.log(`Don't have user Email`)
+}
+// "String have Something" -> True
+// ""-> Empty string is false
+// [] -> Empty array is true
+
+//Falsy Values -> false, 0, -0, BigInt 0n, "", null, undefined, NaN
+//Truthy Values -> Except falsy value such as "0", "false", " ", [], {}, function(){}
+
+const checkArray = [];
+if (checkArray.length === 0){
+  console.log("Array is Empty"); // Array is Empty
+}
+
+const emptyObj = {};
+if(Object.values(emptyObj).length === 0){
+  console.log("Object is Empty"); // Object is Empty
+}
+
+// false == 0 -> True
+// false == '' -> True
+// 0 == '' -> True
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10 // 5
+// val1 = null ?? 10 // 10
+// val1 = undefined ?? 15  // 15
+val1 = null ?? 10 ?? 15 // 10
+
+// To prevent fallback from errors in code execution
+console.log(val1);
+
+//Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice >= 80 ? console.log("greater than 80") : console.log("Less than 80")
